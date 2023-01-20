b4263c3b74029a731f8596d762ec7df99ac7f6f7
swaroopzee <107682853+swaroopzee@users.noreply.github.com>
2023-01-20 12:19:49 +0530

    React setup with redux integration

---
+++ zee5-app/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "zee5-store/store";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(store, "store");
+root.render(
+  <Provider store={store}>
+    {/* <PersistGate persistor={persistor}> */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    {/* </PersistGate> */}
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
